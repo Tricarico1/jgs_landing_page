d71ca5adac3c0a1fd3e81f1c15ccc179ba57a587
Moog <tpieller@gmail.com>
2023-05-19 02:35:43 -0400

    added parallax header to home page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
+import '@/styles/index.scss';
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
