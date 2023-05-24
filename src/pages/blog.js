e38e1facc65e42e7b5fe18b7410b3dc3474f5c59
Moog <tpieller@gmail.com>
2023-05-24 17:13:22 -0400

    added blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '@/components/header';
+
+export default function Blog() {
+  return (
+    <div className="page-wrapper bg-rosebrown">
+      <Header />
+
+      <div className="blog-container">
+        
+      </div>
+        
+    </div>
+  )
+}
